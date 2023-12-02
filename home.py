6cb3b6de349168aedcef7197d580e075ffd707fb
admin <yachen5@yahoo.com>
2023-12-02 17:38:40 +0800

    add sidebar and more

--- home.py
+++ home.py
@@ -14,17 +14,7 @@ import pickle
 pkl_file_path = r'C:\Users\admin\Documents\Python Scripts\HK Analysis\summary.pkl'
 
 
-def layout_main(name):
-    col1, col2 = st.columns([1, 2])
-    with open(pkl_file_path, 'rb') as pkl_file:
-        # Load the data from the Pickle file
-        a_dic = pickle.load(pkl_file)
-
-    selections = list(a_dic.keys())
-    selections.sort()
-    selections.insert(0, None)
-    a_sel = col1.selectbox("Please select a report", selections)
-
+def layout_main(a_dic, a_sel):
     if a_sel is not None:
         df = a_dic[a_sel]
         # Group by to get % correct by student
@@ -45,6 +35,7 @@ def layout_main(name):
         s_c = s_c.drop(['Answer', 'Question'], axis=1)
         # st.dataframe(s_c)
         fig = px.bar(s_c, x='學號', y='Percentage', color='Rank', width=1200, height=600)
+        fig.update_xaxes(showticklabels=False)
         st.plotly_chart(fig)
         b_len = len(df)
         df = df.merge(s_c, on=['學號'], how='left')
@@ -72,32 +63,57 @@ def layout_main(name):
 
         df_sorted = df_a.sort_values(by='Percentage')
 
-        fig = px.bar(df_sorted, x='Question', y='Percentage', text='percentage_text', width=1200, height=600)
+        fig = px.bar(df_sorted, x='Question', y='Percentage', text='percentage_text', color='Percentage', width=1200,
+                     height=600, color_continuous_scale=["red", "green"])
         st.markdown('### Sorted by 答對率')
         st.plotly_chart(fig)
 
-        with st.form("單一問題分析"):
-            col1, col2, col3 = st.columns(3)
-            # Add form components
-            qs = list(df_1['Question'].unique())
-            qs.sort()
-            a_q = col1.selectbox('請選擇一個題目', qs)
+        st.markdown("### 單一問題分析")
+        col1, col2, col3 = st.columns(3)
+        # Add form components
+        qs = list(df_sorted['Question'].unique())
+        # qs.sort()
+        a_q = col1.selectbox('請選擇一個題目', qs)
+        op = [None, '全年級', '分班']
+        a_p = col2.selectbox('分析方法', op)
+        if a_p == '分班':
             cc = list(df['班級'].unique())
             cc.sort()
             # cc.insert(0, 'All')
-            a_c = col2.multiselect('選擇班級，可複選', cc, cc)
-
-            submitted = st.form_submit_button("Submit")
+            a_c = col3.selectbox('選擇班級', cc)
+            # submitted = st.form_submit_button("Submit")
 
         # Display the entered values after form submission
-        if submitted:
-            df_q = df[(df['Question'] == a_q) & (df['班級'].isin(a_c))]
-            st.dataframe(df_q)
+        if a_p is not None:
+            if a_p == '分班':
+                # cc = list(df['班級'].unique())
+                # cc.sort()
+                # # cc.insert(0, 'All')
+                # a_c = st.selectbox('選擇班級', cc)
+                df_q = df[(df['Question'] == a_q) & (df['班級'] == a_c)]
+                st.dataframe(df_q)
+            else:
+                grouped_df = df.groupby(['年級', '班級', 'Question', 'Answer']).agg({'學號': 'count'})
+                grouped_df['Percentage'] = grouped_df.groupby(['年級', '班級', 'Question'])['學號'].transform(
+                    lambda x: x / x.sum())
+                grouped_df = grouped_df.reset_index()
+                grouped_df['percentage_text'] = grouped_df['Percentage'].apply(lambda x: f'{int(x * 100)}%')
+                # Display the grouped DataFrame with counts
+                st.dataframe(grouped_df)
+
             # st.success(f"Name: {name}, Age: {age}, Email: {email}")
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    layout_main('PyCharm')
+    with open(pkl_file_path, 'rb') as pkl_file:
+        # Load the data from the Pickle file
+        a_dic = pickle.load(pkl_file)
+
+    selections = list(a_dic.keys())
+    selections.sort()
+    selections.insert(0, None)
+    a_sel = st.sidebar.selectbox("Please select a report", selections)
+    layout_main(a_dic, a_sel)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/

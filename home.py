8ccd50b1f859f74992b2bf3a5f527f0f1425c0d1
admin <yachen5@yahoo.com>
2024-02-02 07:22:44 +0800

    expanding to report generation. still developing

--- home.py
+++ home.py
@@ -3,7 +3,8 @@ import streamlit_antd_components as sac
 
 from Subpages import By_Class_Analysis, By_Subject_Analysis, Upload_Excel, Generate_Report
 
-st.set_page_config(layout="wide")
+
+# st.set_page_config(layout="wide")
 
 
 def overview():
